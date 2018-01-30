d246344a129644745cd398e8be4346eea6082558
tristar <tristar@urban-theory.net>
2018-01-30 23:25:00 +0900

    centidon: AWS対応に伴う変更

---
+++ boot.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -sf /volumes/mastodon-assets/assets /mastodon/public/assets
+ln -sf /volumes/mastodon-assets/packs /mastodon/public/packs
+
+
+$@
--- web_boot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-bundle exec rails s -p 3000 -b '0.0.0.0'
